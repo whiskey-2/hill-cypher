3801c6b1c11acb6216b7e1a5eb08949dccb5d5e1
whiskey <43121415+whiskey-2@users.noreply.github.com>
2023-06-21 14:28:07 -0400

    Added main that now works

--- main.py
+++ main.py
@@ -32,7 +32,13 @@ def make_couples(sentence, alphabet):
     couple = []
     couple_matrices = []
     while i < len(sentence):
-        couple.append(alphabet.index(sentence[i]))
+        #Accounts for spaces by replacing them with the letter v
+        #Can be replaced with strip() function, but will decide after feedback
+        if sentence[i] == ' ':
+            couple.append(alphabet.index('v'))
+        else:
+            couple.append(alphabet.index(sentence[i]))
+
         i += 1
         if i%2 == 0:
             couple_matrices.append(couple)
@@ -70,18 +76,6 @@ def encrypt(cypher, couples, alphabet):
         result = mult_mat(cypher, couples[i])
 
         final.append([result[0]%26, result[1]%26])
-
-        ''' This is a failed attempt at taking the "modulus"...There's a function for that already...
-
-        if result[0] > 26 and result[1] <= 26:
-            final.append([result[0] - 26, result[1]])
-        elif result[1] > 26 and result[0] <=26:
-            final.append([result[0], result[1] - 26])
-        elif result[0] > 26 and result[1] > 26:
-            final.append([result[0] - 26, result[1] - 26])
-        else:
-            final.append([result[0], result[1]])
-        '''
             
         i += 1
     
@@ -102,12 +96,11 @@ def merge_letters(letter_couples):
         temp = ''.join(letter_couples[i])
         merging.append(temp)
     merged = ''.join(merging)
-    print(merged)
+    return merged
 
 def inverse_mat(matrix):
     determinant = matrix[0]*matrix[3] - matrix[1]*matrix[2]
     det_mod = pow(determinant, -1, 26)
-    print(det_mod)
     inverse_matrix = [(matrix[3]*det_mod)%26, (-matrix[1]*det_mod)%26, (-matrix[2]*det_mod)%26, (matrix[0]*det_mod)%26]
 
     return inverse_matrix
@@ -120,8 +113,6 @@ def decrypt(cypher, enc_couples, alphabet):
 
     while i < len(enc_couples):
         result = mult_mat(cypher, enc_couples[i])
-        print(result)
-
         decrypted.append([result[0]%26, result[1]%26])
         i += 1
 
@@ -136,41 +127,52 @@ def decrypt(cypher, enc_couples, alphabet):
     
     return [decrypted, letters]
 
+def get_cypher_user():
+    joined = [0,0,0,0]
+    joined[0] = int(input("What is the first value of the encrypting matrix? "))
+    joined[1] = int(input("What is the second value of the encrypting matrix? "))
+    joined[2] = int(input("What is the thrid value of the encrypting matrix? "))
+    joined[3] = int(input("What is the fourth value of the encrypting matrix? "))
+
+    return joined
+
+
 def main():
     alphabet = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
     cypher = generate_cypher()
-    sentence = input("Sentence: ") 
-    couple = make_couples(sentence.lower(), alphabet)
+
+    print("""Welcome to the all-in-one Hill Cypher tool. 
+    While this encryption method may be old, it still makes the majority of 1st year Engineering students cry, so it must be decent.
+    This program has several options, which you can select using the numbers corresponding to the function:
+    1. Encrypt a message
+    2. Decrypt a message (with a known cipher)
+    3. Crack a cipher and decrypt the message
+    
+    You may also quit by selecting '0'""")
 
     while True:
-        print("""Welcome to the all-in-one Hill Cypher tool. 
-        While this encryption method may be old, it still makes the majority of 1st year Engineering students cry, so it must be decent.
-        This program has several options, which you can select using the numbers corresponding to the function:
-        1. Encrypt a message
-        2. Decrypt a message (with a known cipher)
-        3. Crack a cipher and decrypt the message
-        
-        You may also quit by selecting '0'""")
 
         user = input("\nSelect which function you would like to use: ")
 
-        if user == 1:
-            enc = encrypt(cypher, couple, alphabet)
+        if user == '1':
+            sentence = input("Sentence to encrypt: ").replace(' ', '').lower()
+            couple_enc = make_couples(sentence, alphabet)
+            enc = encrypt(cypher, couple_enc, alphabet)
             mess_enc = merge_letters(enc[1])
             print(f"Your encrypted message is: {mess_enc}")
             print(f"The cipher used to encrypt it is: {cypher}. Share it only with the recipient of the message!")
-        if user == 2:
-            known_cyph = input("What is the cypher used to encrypt the message? ")
-            mess = input("What is the encrypted message? ")
+        if user == '2':
+            known_cyph = get_cypher_user()
+            message = input("Sentence to decrypt: ")
+            couple_dec = make_couples(message.lower(), alphabet)
             inv_cyph = inverse_mat(known_cyph)
-            dec = decrypt(inv_cyph, mess, alphabet)
+            dec = decrypt(inv_cyph, couple_dec, alphabet)
             mess_dec = merge_letters(dec[1])
             print(f"Here is the decrypted message: {mess_dec}")
-        if user == 3:
+        if user == '3':
             pass
-        if user == 0:
+        if user == '0':
             print("Thanks for using Hill Cypher, see you next time")
             break
 
-
-
+main()
